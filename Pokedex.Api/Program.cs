dd83bb97bb914e54d0ef6c585f31796e9f15ba08
Kristian Brimble <kristian.brimble@sonovate.com>
2021-11-12 10:36:02 +0000

    Wire up validators

--- Pokedex.Api/Program.cs
+++ Pokedex.Api/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
+    .AddValidators()
     .AddPokeapiIntegration(builder.Configuration);
 
 var app = builder.Build();
---
+++ Pokedex.CommandsAndQueries/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Pokedex.CommandsAndQueries;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddValidators(this IServiceCollection services)
+    {
+        services.AddTransient<IValidator<GetPokemonIdQuery>, GetPokemonIdQueryValidator>();
+        services.AddTransient<IValidator<GetPokemonDetailsQuery>, GetPokemonDetailsQueryValidator>();
+
+        return services;
+    }
+}
